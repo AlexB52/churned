7e3ef61c5d25513e5afbd2dc33fcb0d87aaaedf6
Alexandre Barret <AlexB52@users.noreply.github.com>
2021-09-17 19:19:22 +1200

    Feature - Setup database (#7)
    * Add active record and sqlite
    
    * Add Thor
    
    * Add activerecord and sqlite to CLI namespace
    
    * WIP -  add dependencies and initiate database
    
    * Install datase on install
    
    * Draft the database population
    
    * Populate database
    
    * Fix ** deprecations

--- lib/churned.rb
+++ lib/churned.rb
@@ -1,6 +1,19 @@
+require "active_record"
+require "sqlite3"
 require "churned/version"
+require "churned/cli"
+
+ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ".churned/database")
 
 module Churned
+  class Commit < ActiveRecord::Base
+    has_many :file_changes
+  end
+
+  class FileChange < ActiveRecord::Base
+    belongs_to :commit
+  end
+
   class Error < StandardError; end
   # Your code goes here...
 end
--- lib/churned/commands/install.rb
+++ lib/churned/commands/install.rb
@@ -11,6 +11,40 @@ module Churned
 
       def execute(input: $stdin, output: $stdout)
         generator.create_dir(".churned")
+        generator.remove_file ".churned/database"
+
+        ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ".churned/database")
+
+        ActiveRecord::Schema.define do
+          create_table :commits, force: true do |t|
+            t.string :sha
+            t.string :author
+          end
+
+          create_table :file_changes, force: true do |t|
+            t.references :commit
+            t.string :pathname
+            t.integer :additions
+            t.integer :deletions
+          end
+        end
+
+        command.run("git log --no-merges --pretty=format:'%H%n%ae' --numstat --since=1.years > .churned/hashes.txt")
+
+        IO.read('.churned/hashes.txt').split("\n\n") do |description|
+          lines = description.split
+
+          sha    = lines.shift
+          author = lines.shift
+          commit = Commit.new(sha: sha, author: author)
+
+          lines.each_slice(3) do |array|
+            additions, deletions, pathname = array
+            commit.file_changes.build(additions: additions, deletions: deletions, pathname: pathname)
+          end
+
+          commit.save
+        end
       end
     end
   end
