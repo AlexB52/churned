309f15882990f500ef78019fbffd72a46d440b62
Alexandre Barret <alex@abletech.nz>
2021-09-17 19:22:20 +1200

    teletype add console

--- lib/churned/cli.rb
+++ lib/churned/cli.rb
@@ -18,6 +18,18 @@ module Churned
     end
     map %w(--version -v) => :version
 
+    desc 'console', 'Command description...'
+    method_option :help, aliases: '-h', type: :boolean,
+                         desc: 'Display usage information'
+    def console(*)
+      if options[:help]
+        invoke :help, ['console']
+      else
+        require_relative 'commands/console'
+        Churned::Commands::Console.new(options).execute
+      end
+    end
+
     desc 'install', 'Creates the \'.churned\' working directory'
     method_option :help, aliases: '-h', type: :boolean,
                          desc: 'Display usage information'
---
+++ test/integration/console_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+require 'churned/commands/console'
+
+class Churned::Commands::ConsoleTest < Minitest::Test
+  def test_executes_churned_help_console_command_successfully
+    output = `churned help console`
+    expected_output = <<-OUT
+Usage:
+  churned console
+
+Options:
+  -h, [--help], [--no-help]  # Display usage information
+
+Command description...
+    OUT
+
+    assert_equal expected_output, output
+  end
+end
