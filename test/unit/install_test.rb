354f10762f0f2dbad30d9d8f06ac516f356c9ac0
Alexandre Barret <alex@abletech.nz>
2021-09-17 17:22:00 +1200

    teletype add install

---
+++ test/unit/install_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+require 'churned/commands/install'
+
+class Churned::Commands::InstallTest < Minitest::Test
+  def test_executes_install_command_successfully
+    output = StringIO.new
+    options = {}
+    command = Churned::Commands::Install.new(options)
+
+    command.execute(output: output)
+
+    assert_equal "OK\n", output.string
+  end
+end
