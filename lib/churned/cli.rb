a514775d51c83c708e0f5f298813cdd900276ee6
Alexandre Barret <alex@abletech.nz>
2021-09-17 17:52:30 +1200

    WIP -  add dependencies and initiate database

--- lib/churned/cli.rb
+++ lib/churned/cli.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 require 'thor'
-require "active_record"
-require "sqlite3"
 
 module Churned
   # Handle the application command line parsing
--- lib/churned/commands/install.rb
+++ lib/churned/commands/install.rb
@@ -11,6 +11,15 @@ module Churned
 
       def execute(input: $stdin, output: $stdout)
         generator.create_dir(".churned")
+
+        ActiveRecord::Schema.define do
+          create_table :posts, force: true do |t|
+          end
+
+          create_table :comments, force: true do |t|
+            t.integer :post_id
+          end
+        end
       end
     end
   end
