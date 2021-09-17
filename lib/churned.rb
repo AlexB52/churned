2029c8870788e0f4c8c5d586011f973f7bb7bf84
Alexandre Barret <alex@abletech.nz>
2021-09-17 18:49:52 +1200

    Draft the database population

--- lib/churned.rb
+++ lib/churned.rb
@@ -3,12 +3,12 @@ require "sqlite3"
 require "churned/version"
 require "churned/cli"
 
-# ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ".churned/database")
-
-class Commit < ActiveRecord::Base
-end
+ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ".churned/database")
 
 module Churned
+  class Commit < ActiveRecord::Base
+  end
+
   class Error < StandardError; end
   # Your code goes here...
 end
--- lib/churned/commands/install.rb
+++ lib/churned/commands/install.rb
@@ -12,12 +12,17 @@ module Churned
       def execute(input: $stdin, output: $stdout)
         generator.create_dir(".churned")
         generator.remove_file ".churned/database"
+
         ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ".churned/database")
+
         ActiveRecord::Schema.define do
-          create_table :commits, force: true do |t|
-            t.string :hash
+          create_table :commits, id: :integer, force: :cascade do |t|
+            t.string :sha
           end
         end
+
+        command.run("git log --no-merges --pretty=format:'%H' --since=1.years > .churned/hashes.txt")
+        IO.readlines('.churned/hashes.txt', chomp: true).each { |line| Commit.create(sha: line)}
       end
     end
   end
